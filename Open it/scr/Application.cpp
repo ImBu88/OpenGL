d908af01aa0ecb9166df66f01954523503d3e6e7
ImBu88 <cevaceva1234@gmail.com>
2020-09-30 18:41:32 +0200

    New One Open GL

---
+++ Open it/scr/Application.cpp
@@ -0,0 +1,126 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "Renderer.h"
+
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "Shader.h"
+#include "Texture.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+//#include <GL/gl.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error";
+    {
+        float positions[] =
+        {
+        -0.5f, -0.5f, 0.0f, 0.0f, // 0 bottom left (last 2 is texture coordinates)
+         0.5f, -0.5f, 1.0f, 0.0f, // 1 bottom right 
+         0.5f,  0.5f, 1.0f, 1.0f, // 2 top right
+        -0.5f,  0.5f, 0.0f, 1.0f  // 3 top left
+        };
+
+        unsigned int indices[] = {
+            0, 1, 2,
+            2, 3, 0
+        };
+
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+        
+        VertexArray va;
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+        //va.AddBuffer(vb); 
+
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        layout.Push<float>(2); // push 2 more floats (texture stuff)
+        va.AddBuffer(vb, layout);
+
+        IndexBuffer ib(indices, 6);
+
+        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f); //4x3 left right bottom top
+
+        Shader shader("res/shaders/Basic.shader");
+        shader.Bind();
+        shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+        shader.SetUniformMat4f("u_MVP", proj);
+
+        Texture texture("res/textures/Exoicon.png");
+        texture.Bind(); // number here = number down; because it is number of a slot.
+        shader.SetUniform1i("u_Texture", 0);
+
+        va.Unbind();
+        vb.Unbind();
+        ib.Unbind();
+        shader.Unbind();
+
+        Renderer renderer;
+
+        float r = 0.0f;
+        float increment = 0.05f;
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Render here */
+            renderer.Clear();
+
+            shader.Bind();
+            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+            renderer.Draw(va, ib, shader);
+
+            if (r > 1.0f)
+                increment = -0.05f;
+            else if (r < 0.0f)
+                increment = 0.05f;
+
+            r += increment;
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+    }
+
+    glfwTerminate();
+    return 0;
+}
